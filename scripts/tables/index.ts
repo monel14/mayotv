32ea1b2b95c5cc3424556c0787982397dabb4cc8
monel14 <zomadigilris@gmail.com>
2025-09-01 15:26:45 +0100

    chore(github): supprimer tous les templates d'issues et workflows GitHub
    - Supprimé tous les modèles d'issues (.github/ISSUE_TEMPLATE)
    - Supprimé les workflows GitHub Actions (.github/workflows)
    - Supprimé le fichier de financement (.github/FUNDING.yml)
    - Supprimé la configuration des issues (.github

--- scripts/tables/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './categoryTable'
-export * from './countryTable'
-export * from './languageTable'
-export * from './regionTable'
-export * from './subdivisionTable'
--- scripts/utils.ts
+++
@@ -1,8 +0,0 @@
-export function isURI(string: string): boolean {
-  try {
-    new URL(string)
-    return true
-  } catch {
-    return false
-  }
-}
